eff58dda8910a913446a6ca55378e1aa6063572e
Lee ChaeYoung <61898513+ChaeChae0505@users.noreply.github.com>
2022-07-18 22:55:40 +0900

    Update factorial_zero_num.py

--- factorial_zero_num/factorial_zero_num.py
+++ factorial_zero_num/factorial_zero_num.py
@@ -1,1 +1,25 @@
+# N!의 값에서 0을 구하기 위해서는 5의 몫을 구하면 됨
+num = int(input())
+cnt = 0
 
+# N! 의 값
+while num >= 5:
+  cnt += num/5
+  num //=5
+print(int(cnt))
+
+
+# 그게 아니라면 재귀함수를 사용해서 factorial을 구하고 str 변환 -> 0 세기
+def factorial(n):
+  return n*factorial(n-1) if n > 1 else 1
+
+num = int(input())
+cnt = 0
+N = str(factorial(num))
+
+for i in range(len(N)-1, -1, -1):
+    if N[i] == '0':
+        cnt += 1
+    else:
+        break
+print(cnt)
